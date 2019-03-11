668db126567dc790261259a6df40de2ecd40958c
Lukas Schwab <lukas.schwab@gmail.com>
2019-03-11 09:51:07 -0700

    Cleanup

--- main.go
+++ main.go
@@ -15,8 +15,6 @@ import (
 
 const sintel = "magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent"
 
-const thereWillBeBlood = "magnet:?xt=urn:btih:50247341e9c86396f26468c80730a494768696e2&dn=There.Will.Be.Blood.2007.1080p.BluRay.x264.anoXmous&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969" // THERE WILL BE BLOOD
-
 const quantum = time.Second / 3
 
 func getMagnet() string {
